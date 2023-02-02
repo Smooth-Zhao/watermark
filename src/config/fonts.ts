f442cde165e714b585c50f0e6e3c7b3b86584f66
Smooth <smoothly-zhao@qq.com>
2023-02-02 14:06:42 +0800

    feat: 新增字体

---
+++ src/config/fonts.ts
@@ -0,0 +1,10 @@
+import microsportbold from "../assets/fonts/microsportbold-yz1zy.ttf"
+import Verilate from "../assets/fonts/verilatereguler-8mdqj.ttf"
+const fonts = [{
+    name:"Microsport bold",
+    font:microsportbold
+},{
+    name:"Verilate",
+    font:Verilate
+}]
+export default fonts
