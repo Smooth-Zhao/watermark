fe8c94dd8b218053634af3d066af73a2d0474b24
Smooth <smoothly-zhao@qq.com>
2023-02-02 14:06:27 +0800

    fix: 按照相片比例来

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
-import {onMounted, reactive, ref} from "vue";
-import Photo from "./assets/default.jpg"
+import {computed, onMounted, reactive, ref} from "vue";
 import NikonLogo from "./assets/nikon.png"
 import SonyLogo from "./assets/sony.png"
 import FujifilmLogo from "./assets/fujifilm.png"
 import AppleLogo from "./assets/apple.png"
 import exifr from 'exifr'
-import PingFangFont from "./assets/pingfang.ttf"
+import PingFangFont from "./assets/fonts/microsportbold-yz1zy.ttf"
 import dayjs from "dayjs";
+import fonts from "./config/fonts";
 const canvasRef = ref<HTMLCanvasElement>()
 let ctx:CanvasRenderingContext2D
 const canvasConfig = reactive({
     width:3000,
     height:2330
 })
-onMounted(()=>{
-    (document.querySelector(".canvas canvas") as HTMLElement).style.transform = `scale(${document.body.clientWidth * 0.5 / 3000})`;
-    (document.querySelector(".canvas img") as HTMLElement).style.transform = `scale(${document.body.clientWidth * 0.5 / 3000})`;
-})
+const proportion = computed(()=>document.body.offsetWidth * (document.body.offsetWidth / document.body.offsetHeight > 1 ? 0.6 : 0.9) / canvasConfig.width)
+// onMounted(()=>{
+//     const proportion =  document.body.offsetWidth * 0.4 / canvasConfig.width;
+//     (document.querySelector(".canvas canvas") as HTMLElement).style.transform = `scale(${proportion})`;
+//     (document.querySelector(".canvas img") as HTMLElement).style.transform = `scale(${(document.querySelector(".canvas") as HTMLElement).offsetWidth / 3000 / 0.65})`;
+// })
 const logo = {
     nikon:NikonLogo,
     sony:SonyLogo,
@@ -29,27 +31,27 @@ async function render(photo:HTMLImageElement){
     document.fonts.add(await font.load())
     ctx = canvasRef.value?.getContext("2d")!
     const standardWidth = canvasConfig.width
-    const logoStandardHeight = 134
+    const logoStandardHeight = 140
+    const imageHeight = canvasConfig.height - 330
     canvasRef.value.width = standardWidth
     canvasRef.value.height = canvasConfig.height
     const padding = 50
     ctx.fillStyle = "white"
     ctx.fillRect(0,0,canvasRef.value.width,canvasRef.value.height)
-    ctx?.drawImage(photo,0,0,standardWidth,standardWidth / 3 * 2)
+    ctx?.drawImage(photo,0,0,standardWidth,imageHeight)
     exifr.parse(photo).then(async output => {
         // 相机型号
         ctx.beginPath()
         ctx.textBaseline = "top"
         ctx.font = `bolder ${standardWidth * 0.02}px PingFangSC-Regular,PingFang,sans-serif`
         ctx.fillStyle = "black"
-        ctx.fillText(`${output.Model}`, padding * 2, standardWidth / 3 * 2 + padding * 2)
+        ctx.fillText(`${output.Model}`, padding * 2, imageHeight + padding * 2)
 
         ctx.beginPath()
         ctx.textBaseline = "top"
         ctx.font = ` ${standardWidth * 0.015}px PingFang, sans-serif`
         ctx.fillStyle = "#625f5f"
-        ctx.fillText(`${output.Make}`, padding * 2, standardWidth / 3 * 2 + padding * 2 + standardWidth * 0.03)
-        console.log(output)
+        ctx.fillText(`${output.Make}`, padding * 2, imageHeight + padding * 2 + standardWidth * 0.03)
         // 相片参数
         const params = `${output.FocalLengthIn35mmFormat}mm f/${output.FNumber} ISO${output.ISO} ${output.ShutterSpeedValue > 0 ? '1/' + (Math.pow(2,output.ShutterSpeedValue) << 0):output.ExposureTime}s ${output.ExposureProgram.charAt(0)}`
         ctx.beginPath()
@@ -60,7 +62,7 @@ async function render(photo:HTMLImageElement){
         ctx.fillText(
             params,
             standardWidth - textWidth - padding * 2,
-            standardWidth / 3 * 2 + padding * 2
+            imageHeight + padding * 2
         )
 
         // logo
@@ -73,7 +75,7 @@ async function render(photo:HTMLImageElement){
         ctx.fillText(
             `${dayjs(output.DateTimeOriginal).format("YYYY/MM/DD hh:mm")}`,
             standardWidth - textWidth - padding * 2,
-            standardWidth / 3 * 2 + padding * 2 + standardWidth * 0.03
+            imageHeight + padding * 2 + standardWidth * 0.03
         )
         ;(document.querySelector(".canvas img") as HTMLImageElement).src = canvasRef.value?.toDataURL("image/jpeg")
 
@@ -101,8 +103,8 @@ async function render(photo:HTMLImageElement){
                 const logoWidth = logoImage.width / logoImage.height * logoStandardHeight
                 ctx?.drawImage(
                     logoImage,
-                    standardWidth - textWidth - logoWidth - padding * 3.5,
-                    standardWidth / 3 * 2 + padding * 2,
+                    standardWidth - textWidth - logoWidth - padding * 3,
+                    imageHeight + padding * 2 - 12,
                     logoWidth,
                     logoStandardHeight
                 )
@@ -123,6 +125,8 @@ function onFileChange(event:InputEvent){
         if (e.target?.result){
             image.src = e.target.result as string
             image.onload = ()=>{
+                canvasConfig.height = Math.round(canvasConfig.width / image.width * image.height) + 330
+                console.log(canvasConfig.width , image.width , image.height)
                 render(image)
             }
         }
@@ -135,10 +139,9 @@ function onFileChange(event:InputEvent){
 
 <template>
     <div class="canvas">
-        <canvas ref="canvasRef"></canvas>
-        <img src="">
+        <canvas ref="canvasRef" :style="{transform: `scale(${proportion})`}"></canvas>
+        <img src="" :style="{transform: `scale(${proportion})`}" alt="" @click="handleSelectImage">
     </div>
-    <div class="btn-select-image" @click="handleSelectImage">选择图片</div>
     <input ref="fileRef" @change="onFileChange" type="file" name="" id="" style="width: 0;height: 0;opacity: 0">
 </template>
 
